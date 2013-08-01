ddcd3ae687815522625c339f09b266688cd0caa8
beru222 <Kenichi.beru222.Koyama@gmail.com>
2013-08-01 13:50:29 +0900

    add PHP Coding Standards Fixer execute
    add PHP Coding Standards Fixer execute.

---
+++ AddPATH.bat
@@ -0,0 +1,28 @@
+@echo off
+rem #####
+rem # this bat file add path own directory.
+rem #
+rem #####
+set PJ_HOME=%cd%
+pushd ..\..\xampp\php
+set BIN=%cd%
+set PATH=%PATH%;%BIN%
+echo %PATH%
+pause
+rem php.exe php-cs-fixer.phar self-update
+
+popd
+for %%f in (*.php) do (
+    php.exe php-cs-fixer.phar fix %~dp0%%%f
+)
+
+pushd .\tests
+for %%f in (*.php) do (
+    php.exe ..\php-cs-fixer.phar fix %PJ_HOME%\tests\%%f
+)
+
+popd
+
+pause
+
+exit 0
